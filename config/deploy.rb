38bf1295b2b631fde1fe37c3e19cd539f1bb2691
juanmalambre <juanmlambre@gmail.com>
2019-09-16 16:57:49 -0300

    Add rspec to Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application,     'myhealthapp'
-set :repo_url,        'git@github.com:lucianoMintrone/myhealthapp-ws.git'
+set :repo_url,        'git@github.com:JuanmaLambre/myhealth-serv.git'
 set :deploy_to, '/srv/rails/my_health_app'
 set :keep_releases, 2
 set :linked_dirs, fetch( :linked_dirs, [] ).push( 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads' )
