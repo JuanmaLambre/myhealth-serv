ea50a64e4b101a84f6d9a22c0152eeaeb2823335
Luciano Mintrone <luciano@theamalgama.com>
2019-11-09 19:50:30 -0300

    Close #12 - Filter HealthProviders by current user medical plan

--- app/controllers/health_providers_controller.rb
+++ app/controllers/health_providers_controller.rb
@@ -3,6 +3,8 @@ class HealthProvidersController < ApplicationController
 
 	def search
 		health_providers = HealthProvider.where(specialty_id: params[:specialty_id])
+			.includes(:medical_plans_accepted).references(:medical_plans_accepted)
+			.where(medical_plans: { id: current_user.medical_plan.id } )
 			.within(params[:radius], :origin => [params[:latitude], params[:longitude]])
 		render_collection health_providers
 	end
